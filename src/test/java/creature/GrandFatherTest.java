8c93d2a11db473eb875ebb1bbf4b419cc388d6cc
cjinchi <cjinchi@qq.com>
2018-12-31 13:16:28 +0800

    fix fix test bug

--- src/test/java/creature/GrandFatherTest.java
+++ src/test/java/creature/GrandFatherTest.java
@@ -1,16 +1,19 @@
 package creature;
 
+import javafx.application.Application;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class GrandFatherTest {
+    //for disable error "Internal graphics not initialized yet"
+    JFXPanel jfxPanel = new JFXPanel();
+
     @Test
     public void grandFatherGetInstance(){
-        //disable error "Internal graphics not initialized yet"
-        JFXPanel jfxPanel = new JFXPanel();
-
         GrandFather base = GrandFather.getInstance();
         for(int i=0;i<5;i++){
             new Thread(()->{
